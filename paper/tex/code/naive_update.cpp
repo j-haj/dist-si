e480325c6696f04594ae3c592e468a2800e23ce3
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-01-29 14:11:39 -0600

    Adds code directory

---
+++ paper/tex/code/naive_update.cpp
@@ -0,0 +1,11 @@
+// Inside a for-loop with index variable i
+const best_fitness = fitness_(gbest_);
+const double fitness = fitness_(positions_[i]);
+if (fitness < fitness_(best_positions_[i])) {
+  best_positions_[i] = positions_[i];
+#pragma omp critical
+  if (fitness < best_fitness) {
+    gbest_ = positions_[i];
+    best_fitness = fitness;
+  }
+ }
